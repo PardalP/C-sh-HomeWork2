ee51d1d19ed4a47a3ac02cf2a961d8341b8ec8db
Pardal <pardal2608@gmail.com>
2023-04-28 13:16:41 +0600

    First task

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = (num / 10) % 10;
+Console.WriteLine("Вторая цифра введенного числа: " + res);
